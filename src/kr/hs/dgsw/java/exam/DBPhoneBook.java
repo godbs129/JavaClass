d520722164d3332ae36ad64c21e2a02438b95c09
전해윤 <junhaeyoon@gmail.com>
2021-06-09 14:18:00 +0900

    2021-06-09

---
+++ src/kr/hs/dgsw/java/exam/DBPhoneBook.java
@@ -0,0 +1,198 @@
+package kr.hs.dgsw.java.exam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBPhoneBook implements PhoneBook {
+    private Connection getConnection() throws Exception {
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        return DriverManager.getConnection("jdbc:mariadb://211.53.209.159/dgsw_java", "dgsw_student", "1234");
+    }
+
+    public NameCard makeCard(ResultSet rs) throws Exception {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String phoneNum = rs.getString("phone_number");
+        String address = rs.getString("address");
+
+        NameCard card = new NameCard();
+        card.setId(id);
+        card.setName(name);
+        card.setPhoneNumber(phoneNum);
+        card.setAddress(address);
+
+        return card;
+    }
+
+    @Override
+    public List<NameCard> getList() {
+        try {
+            Connection con = getConnection();
+
+            String Sql = "select * from phone_book";
+            PreparedStatement preparedStatement = con.prepareStatement(Sql);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            List<NameCard> list = new ArrayList<NameCard>();
+            NameCard card = null;
+            while (rs.next()) {
+                card = makeCard(rs);
+
+                list.add(card);
+            }
+
+            con.close();
+            preparedStatement.close();
+
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public NameCard getCard(int id) {
+        try {
+            Connection con = getConnection();
+
+            String sql = "select * from phone_book where id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            NameCard card = null;
+            if (rs.next()) {
+                card = makeCard(rs);
+            }
+
+            con.close();
+            preparedStatement.close();
+
+            return card;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public NameCard getCard(String name) {
+        try {
+            Connection con = getConnection();
+
+            String sql = "select * from phone_book where name = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            NameCard card = null;
+            if (rs.next()) {
+                card = makeCard(rs);
+            }
+
+            con.close();
+            preparedStatement.close();
+
+            return card;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int addCard(String name, String phoneNumber, String address) {
+        try {
+            Connection con = getConnection();
+
+            StringBuilder sql = new StringBuilder();
+            sql.append("insert into phone_book");
+            sql.append(" (name, phone_number, address) ");
+            sql.append("values ");
+            sql.append(" (?, ?, ?)");
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql.toString());
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, phoneNumber);
+            preparedStatement.setString(3, address);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+            con.close();
+
+            return 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    @Override
+    public boolean removeCard(int id) {
+        try {
+            Connection con = getConnection();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("delete from phone_book");
+            stringBuilder.append(" where id = ?");
+
+            PreparedStatement preparedStatement = con.prepareStatement(stringBuilder.toString());
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+            con.close();
+
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void updateCard(int id, String name, String phoneNumber, String address) {
+        try {
+            Connection con = getConnection();
+
+            StringBuilder sql = new StringBuilder();
+            sql.append("update phone_book set ");
+            sql.append(" name = ?, ");
+            sql.append("  phone_number = ?, ");
+            sql.append("  address = ? ");
+            sql.append(" where id = ?");
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql.toString());
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, phoneNumber);
+            preparedStatement.setString(3, address);
+            preparedStatement.setInt(4, id);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int size() {
+        List<NameCard> list = this.getList();
+        return list.size();
+    }
+}
--- src/kr/hs/dgsw/java/exam/PhoneBookTest.java
+++ src/kr/hs/dgsw/java/exam/PhoneBookTest.java
@@ -6,14 +6,14 @@ public class PhoneBookTest {
 
 	public static void main(String[] args) {
 		
-		PhoneBook phoneBook = new FilePhoneBook();
+		PhoneBook phoneBook = new DBPhoneBook();
 		NameCard nameCard = null;
 
 		// 네 개의 데이터를 추가하세요.
-		phoneBook.addCard("정재봉", "010-3458-4250", "대구시 동구");
-		phoneBook.addCard("유재석", "010-1234-5678", "서울시 강남구");
-		phoneBook.addCard("손흥민", "010-0000-0000", "런던시 토튼햄구");
-		phoneBook.addCard("윤여정", "010-9876-5432", "아카데미 시상식장");
+//		phoneBook.addCard("정재봉", "010-3458-4250", "대구시 동구");
+//		phoneBook.addCard("유재석", "010-1234-5678", "서울시 강남구");
+//		phoneBook.addCard("손흥민", "010-0000-0000", "런던시 토튼햄구");
+//		phoneBook.addCard("윤여정", "010-9876-5432", "아카데미 시상식장");
 
 
 		// 등록된 모든 연락처를 읽어와 상세 정보를 출력하세요.
@@ -34,12 +34,12 @@ public class PhoneBookTest {
 		System.out.println(nameCard.toString());
 
 		// 세번째로 등록된 연락처를 삭제하세요.
-		phoneBook.removeCard(cards.get(2).getId());
+		phoneBook.removeCard(cards.get(0).getId());
 		System.out.println("removeCard() --------------------");
 		System.out.println("size : " + phoneBook.size());
 
 		// 두번째로 등록된 연락처의 정보를 임의로 변경하세요.
-		phoneBook.updateCard(cards.get(1).getId(), "유산슬", "010-5555-6666", "합정역 5번 출구");
+		phoneBook.updateCard(cards.get(1).getId(), "유2산슬", "010-5555-6666", "합정역 5번 출구");
 		System.out.println("updateCard() --------------------");
 		nameCard = phoneBook.getCard(cards.get(1).getId());
 		System.out.println(nameCard.toString());
